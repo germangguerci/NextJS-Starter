69023ea30caf546284bb950792031a71b1230d56
German <germangguerci@gmail.com>
2023-10-30 12:09:01 -0600

    feat(app): added language selector to / page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,6 +2,7 @@
 
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import LanguageSelect from "./components/LanguageSelect";
 
 export default function Home() {
   const t = useTranslations("Home");
@@ -13,8 +14,9 @@ export default function Home() {
           <code className="font-mono font-bold">&nbsp;src/app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <LanguageSelect />
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            className="pointer-events-none ml-4 flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
